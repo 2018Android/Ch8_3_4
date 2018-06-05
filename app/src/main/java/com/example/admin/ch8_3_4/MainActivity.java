de84e4b54fe59eeb1f76eafc1bebab83bac971e0
chenyirou <annie851216@gmail.com>
2018-06-05 11:56:15 +0800

    TimePickerDialog 方法

--- app/src/main/java/com/example/admin/ch8_3_4/MainActivity.java
+++ app/src/main/java/com/example/admin/ch8_3_4/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_datefragment = (Button)findViewById(R.id.btn_DateFragment);
+        btn_datefragment.setOnClickListener(btn_dateListener);
+
+        Button btn_timefragment = (Button)findViewById(R.id.btn_TimeFragment);
+        btn_timefragment.setOnClickListener(btn_timeListener);
     }
 
     public void getDate(int year, int monthOfYear, int dayOfMonth){
         TextView txv_Date = (TextView)findViewById((R.id.txv_date));
-        txv_Date.setText(Integer.toString(year)+"/"+Integer.toString(monthOfYear)+"/"+Integer.toString(dayOfMonth));
+        txv_Date.setText(Integer.toString(year)+"/"+Integer.toString(monthOfYear+1)+"/"+Integer.toString(dayOfMonth));
     }
 
     View.OnClickListener btn_dateListener = new View.OnClickListener(){
@@ -24,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
             DatePickerDialogFragment dlg = DatePickerDialogFragment.newInstance();
             FragmentManager fm = getSupportFragmentManager();
             dlg.show(fm, "datepickerdialog");
+        }
+    };
+
+    public void getTime(int hourOfDay, int minute){
+        TextView txv_time = (TextView)findViewById(R.id.txv_time);
+        txv_time.setText(Integer.toString(hourOfDay)+"："+Integer.toString(minute));
+    }
+
+    View.OnClickListener btn_timeListener = new View.OnClickListener(){
+        public void onClick(View v){
+            TimePickerDialogFragment dlg = TimePickerDialogFragment.newInstance();
+            FragmentManager fm = getSupportFragmentManager();
+            dlg.show(fm, "timepickerdialog");
         }
     };
 }
--- app/src/main/java/com/example/admin/ch8_3_4/TimePickerDialogFragment.java
+++ app/src/main/java/com/example/admin/ch8_3_4/TimePickerDialogFragment.java
@@ -1,17 +1,47 @@
 package com.example.admin.ch8_3_4;
 
 
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TimePickerDialogFragment extends Fragment {
+public class TimePickerDialogFragment extends DialogFragment {
+
+    Calendar dt = Calendar.getInstance();
+    static TimePickerDialogFragment newInstance(){
+        TimePickerDialogFragment dlg = new TimePickerDialogFragment();
+        return dlg;
+    }
+
+    TimePickerDialog.OnTimeSetListener tpdListener = new TimePickerDialog.OnTimeSetListener(){
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+            ((MainActivity)getActivity()).getTime(hourOfDay, minute);
+        }
+    };
+
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        TimePickerDialog tDialog = new TimePickerDialog(
+                getActivity()
+                , tpdListener
+                , dt.get(Calendar.HOUR)
+                , dt.get(Calendar.MINUTE)
+                ,true
+        );
+        return tDialog;
+    }
 
 
     public TimePickerDialogFragment() {
